b8c45f97500449cbc8509fb87bafaaed51363b7c
fftl <fftl0785@gmail.com>
2025-04-09 06:03:46 +0900

    update : get json ready

---
+++ src/main/java/com/fftl/law_alarm_back/dto/LawSearch.java
@@ -0,0 +1,10 @@
+package com.fftl.law_alarm_back.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class LawSearch {
+    private List<Law> laws;
+}
--- src/main/java/com/fftl/law_alarm_back/service/GetLawData.java
+++ src/main/java/com/fftl/law_alarm_back/service/GetLawData.java
@@ -1,27 +1,31 @@
 package com.fftl.law_alarm_back.service;
 
+import com.fftl.law_alarm_back.dto.LawSearch;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
 import java.io.*;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class GetLawData {
 
-    //TODO
-    //properties 에 키넣기
-
-    String key = "";
+    @Value("${law.key}")
+    private String lawKey;
 
     public void getData(){
-        WebClient client = WebClient.builder().baseUrl("http://www.law.go.kr/DRF/lawSearch.do?OC="+key).build();
-
+//        WebClient client = WebClient.builder().baseUrl("http://www.law.go.kr/DRF/lawSearch.do?OC="+lawKey).build();
+//
+//        client.post()
+//                .uri("type=json&query=소방")
+//                .body( );
     }
 
 }
